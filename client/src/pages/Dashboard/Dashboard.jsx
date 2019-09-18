b1f1665b75086a2772349f6aed21e898da2d5200
ebru <ebky14@gmail.com>
2019-09-18 23:14:25 +0300

    redux thunk is applied for handling async calls

--- client/src/pages/Dashboard/Dashboard.jsx
+++ client/src/pages/Dashboard/Dashboard.jsx
@@ -1,53 +1,16 @@
-import React, { useEffect } from 'react';
+import React from 'react';
 import { connect } from 'react-redux';
 import { createStructuredSelector } from 'reselect';
-import axios from 'axios';
-import Cookies from 'js-cookie';
 
 import MostPlayed from '../../components/MostPlayed/MostPlayed';
 
-import { updateUser } from '../../redux/user/user.actions';
-import { selectCurrentUser } from '../../redux/user/user.selectors';
+import { fetchUserStartAsync } from '../../redux/user/user.actions';
+import { selectCurrentUser, selectIsUserFetching } from '../../redux/user/user.selectors';
 
 import './Dashboard.scss';
 
 const Dashboard = props => {
-  const { updateUser, currentUser: { mostPlayed } } = props;
-
-  useEffect(() => {
-    const getUser = async () => {
-      const accessToken = Cookies.get('userToken')
-      const config = {
-        headers: { 'Authorization': 'Bearer ' + accessToken }
-      };
-
-      let userResponse = await axios.get('https://api.spotify.com/v1/me', config);
-      const userData = userResponse.data;
-
-      let mostPlayedResponse = await axios.get('https://api.spotify.com/v1/me/top/tracks?time_range=long_term&limit=1', config);
-      const mostPlayedData = mostPlayedResponse.data.items[0];
-
-      const user = {
-        'sid': userData.id,
-        'name': userData.display_name,
-        'country': userData.country,
-        'profileImage': userData.images[0].url,
-        'profileUrl': userData.uri,
-        'mostPlayed': {
-          'sid': mostPlayedData.id,
-          'name': mostPlayedData.name,
-          'artistName': mostPlayedData.artists[0].name,
-          'previewUrl': mostPlayedData.preview_url,
-          'coverImage': mostPlayedData.album.images[0].url
-        }
-      }
-
-      updateUser(user);
-    }
-
-    // Fetch user info from spotify
-    getUser()
-  }, []);
+  const { isUserFetching, currentUser: { mostPlayed } } = props;
 
   return (
     <div className='Dashboard'>
@@ -61,12 +24,13 @@ const Dashboard = props => {
 };
 
 const mapStateToProps = createStructuredSelector({
+  isUserFetching: selectIsUserFetching,
   currentUser: selectCurrentUser
 });
 
 const mapDispatchToProps = dispatch => {
   return {
-    updateUser: user => dispatch(updateUser(user))
+    fetchUserStartAsync: () => dispatch(fetchUserStartAsync())
   }
 };
 
--- client/src/redux/user/user.reducer.js
+++ client/src/redux/user/user.reducer.js
@@ -1,28 +1,28 @@
 import { userActionTypes } from './user.types';
 
 const initialState = {
-    currentUser: {
-        'sid': '',
-        'name': '',
-        'country': '',
-        'profileImage': '',
-        'profileUrl': '',
-        mostPlayed: {
-            'sid': '',
-            'name': '',
-            'artistName': '',
-            'previewUrl': '',
-            'coverImage': ''
-        }
-    }
+    currentUser: null,
+    isFetching: false
 }
 
 const reducer = (state = initialState, action) => {
     switch (action.type) {
-        case userActionTypes.UPDATE_USER:
+        case userActionTypes.FETCH_COLLECTION_START:
             return {
                 ...state,
+                isFetching: true
+            };
+        case userActionTypes.FETCH_COLLECTION_SUCCESS:
+            return {
+                ...state,
+                isFetching: false,
                 currentUser: action.payload
+            };
+        case userActionTypes.FETCH_COLLECTION_FAILURE:
+            return {
+                ...state,
+                isFetching: false,
+                errorMessage: action.payload
             };
         default:
             return state;
