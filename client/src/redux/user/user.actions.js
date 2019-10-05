0e9bf39994cee7832494fb340a232e17f1c628af
ebru <ebky14@gmail.com>
2019-10-05 14:41:35 +0300

    empty profile image bug fix

--- client/src/redux/user/user.actions.js
+++ client/src/redux/user/user.actions.js
@@ -34,17 +34,17 @@ export const fetchUserStartAsync = () => {
             const mostPlayedData = mostPlayedResponse.data.items[0];
 
             const user = {
-                'sid': userData.id,
-                'name': userData.display_name,
-                'country': userData.country,
-                'profileImage': userData.images[0].url,
-                'profileUrl': userData.uri,
-                'mostPlayed': {
-                    'sid': mostPlayedData.id,
-                    'name': mostPlayedData.name,
-                    'artistName': mostPlayedData.artists[0].name,
-                    'previewUrl': mostPlayedData.preview_url,
-                    'coverImage': mostPlayedData.album.images[0].url
+                sid: userData.id ? userData.id : null,
+                name: userData.display_name ? userData.display_name : null,
+                country: userData.country ? userData.country : null,
+                profileImage: userData.images[0] ? userData.images[0].url : null,
+                profileUrl: userData.uri ? userData.uri : null,
+                mostPlayed: {
+                    sid: mostPlayedData.id ? mostPlayedData.id : null,
+                    name: mostPlayedData.name ? mostPlayedData.name : null,
+                    artistName: mostPlayedData.artists[0] ? mostPlayedData.artists[0].name : null,
+                    previewUrl: mostPlayedData.preview_url ? mostPlayedData.preview_url : null,
+                    coverImage: mostPlayedData.album.images[0] ? mostPlayedData.album.images[0].url : null
                 }
             };
 
