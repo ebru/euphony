8bf5eedbd78d9ff7323ffc9ab034747e9ca22936
ebru <ebky14@gmail.com>
2019-10-16 10:34:19 +0300

    minor fix

--- service/server.js
+++ service/server.js
@@ -27,7 +27,7 @@ mongoose.connection.once('open', () => {
 app.use('/api/graphql', graphqlHTTP({
     schema: schema,
     // Disable graphiql on production
-    graphiql: true
+    graphiql: process.env.NODE_ENV === 'development'
 }));
 
 // Use routes
